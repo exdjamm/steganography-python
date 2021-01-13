6f1af51a4ed2d6870534f3aedfbf5f8cd72c9c1e
exodojaffar <exodojaffar@gmail.com>
2021-01-12 20:20:47 -0400

    Add a limit of how many bits will get to message

--- read-message.py
+++ read-message.py
@@ -1,12 +1,18 @@
 from utils import *
 
-def get_two_bytes_significante(byte_array: bytearray) -> bytearray:
+def get_two_bytes_significante(byte_array: bytearray, len_message: int) -> bytearray:
 	result_bytearray = bytearray()
 
+	count_byte = 0
 	for byte in byte_array:
+		if count_byte == len_message -1:
+			break
+
 		two_byte = 0x3 & byte
 
 		result_bytearray.append(two_byte)
+
+		count_byte += 1
 
 	return result_bytearray
 
@@ -26,11 +32,14 @@ def bytes_message_to_text(byte_message: bytearray) -> str:
 	return result_bytearray.decode('utf-8')
 
 if __name__ == '__main__':
+	len_message = int(input("Quantos caracteres tem a mensagem? >>> "))
+	len_message *= 4
+
 	info_file = ('output/img_message', 'jpeg')
 
 	byte_array_file = read_bytes_file(info_file)
-	byte_array_message = get_two_bytes_significante(byte_array_file)
-
+	byte_array_message = get_two_bytes_significante(byte_array_file, len_message)
+	
 	message = bytes_message_to_text(byte_array_message)
 	print(message)
 	
